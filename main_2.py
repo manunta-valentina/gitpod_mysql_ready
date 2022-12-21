e9efd0894413e511c12b97af1802ba2cd8682ae9
manunta-valentina <manunta.valentina.gioia@iisgalvanimi.edu.it>
2022-12-21 11:26:41 +0000

    es.2

--- main_2.py
+++ main_2.py
@@ -35,3 +35,52 @@ mycursor.execute("SHOW TABLES") #Puoi controllare se esiste una tabella elencand
 
 for x in mycursor:
   print(x)
+
+###########################################################################################################################################################
+ 
+  import mysql.connector
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="yourusername",
+  password="yourpassword",
+  database="mydatabase"
+)
+
+mycursor = mydb.cursor()
+
+mycursor.execute("CREATE TABLE customers (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), address VARCHAR(255))")
+#Quando crei una tabella, dovresti anche creare una colonna con una chiave univoca per ogni record.
+#Questo può essere fatto definendo una PRIMARY KEY.
+#Usiamo l'istruzione "INT AUTO_INCREMENT PRIMARY KEY" che inserirà un numero univoco per ogni record. A partire da 1 e aumentato di uno per ogni record.
+
+###########################################################################################################################################################
+
+import mysql.connector
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="yourusername",
+  password="yourpassword",
+  database="mydatabase"
+)
+
+mycursor = mydb.cursor()
+
+mycursor.execute("ALTER TABLE customers ADD COLUMN id INT AUTO_INCREMENT PRIMARY KEY")
+
+###########################################################################################################################################################
+
+import mysql.connector
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="yourusername",
+  password="yourpassword",
+  database="mydatabase"
+)
+
+mycursor = mydb.cursor()
+
+mycursor.execute("ALTER TABLE customers ADD COLUMN id INT AUTO_INCREMENT PRIMARY KEY") #Se la tabella esiste già, utilizzare la parola chiave ALTER TABLE
+#Crea chiave primaria su una tabella esistente:
